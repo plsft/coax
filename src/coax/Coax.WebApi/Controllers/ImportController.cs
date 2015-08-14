acdacf7901e2873b1e01e5f89887027417e8a49f
plsft <george@plsft.com>
2015-08-14 16:38:19 -0400

    latest build

---
+++ src/coax/Coax.WebApi/Controllers/ImportController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using AttributeRouting.Web.Http;
+
+namespace Coax.WebApi.Controllers
+{
+    public class ImportController : ApiControllerBase
+    {
+        [GET("/import/test")]
+        [HttpGet]
+        public HttpResponseMessage Test()
+        {
+            return Request.CreateResponse(HttpStatusCode.Accepted, new { test = true }, new JsonMediaTypeFormatter());
+        }
+    }
+}
---
+++ src/coax/Coax.WebApi/Controllers/MobileController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using AttributeRouting.Web.Http;
+
+namespace Coax.WebApi.Controllers
+{
+    public class MobileController : ApiControllerBase
+    {
+        [GET("/mobile/test")]
+        [HttpGet]
+        public HttpResponseMessage Test()
+        {
+            return Request.CreateResponse(HttpStatusCode.Accepted, new  {test =true }, new JsonMediaTypeFormatter()); 
+        }
+        
+    }
+}

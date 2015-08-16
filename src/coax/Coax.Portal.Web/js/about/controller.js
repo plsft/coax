41c6fbb1084a54f69475d927c6ed2e27655a5f47
plsft <george@plsft.com>
2015-08-16 01:28:26 -0400

    latest

--- src/coax/Coax.Portal.Web/js/about/controller.js
+++ src/coax/Coax.Portal.Web/js/about/controller.js
@@ -4,16 +4,42 @@ angular.module('About')
     .controller('AboutController',
     [
         '$scope', '$cookieStore', '$log',
-        function($scope, $cookieStore, $log) {
+        function ($scope, $cookieStore, $log) {
             $scope.currentUser = $cookieStore.get('login_user');
             $scope.item = {};
             $scope.items = ['item1', 'item2', 'item3'];
             $log.log('test');
+
+            $scope.OK = function () {
+                $log.log('ok in testController');
+            };
         }
+
+
     ])
     .controller('TestController', 
-    ['$log', function($log) {
-        $log.log('in testController');
-            }]
-    );
+    ['$log', '$http',  '$scope', '$interval',
+function ($log, $http, $scope, $interval) {
+            $log.log('in testContller');
+            $http.get('humans.txt')
+                .then(function (response) {
+                    $interval(function() {
+                            $scope.response = response;
+                            $log.log('in interval');
+                        }, 4000)
+                        .finally(function() {
+                            $scope.dataLoading = false;
+                            $log.log('in finally');
+                        });
+
+                }, function() {
+                $scope.dataLoading = false;
+                $scope.response = "error";
+                $log.log('in error');
+            });
+            
+                
+        }
+        
+    ]);
 

eca279ec6cb29646441c326b7ad288a377b9a056
plsft <george@plsft.com>
2015-09-03 10:25:22 -0400

    latest

---
+++ src/Helix/Utility/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Security;
+
+namespace Helix.Utility
+{
+    public sealed class Auth
+    {
+        public static string GetAuthTicket(int version, string userName, string userData, int expiresInMin = 30, bool persistent = true)
+        {
+            var ticket = new FormsAuthenticationTicket(version, userName, DateTime.Now, DateTime.Now.AddMinutes(expiresInMin), persistent, userData);
+            return FormsAuthentication.Encrypt(ticket);
+        }
+    }
+}
